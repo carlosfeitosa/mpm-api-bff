a575c11b69fd9ccbeb31577945e02efa9017475f
Carlos Feitosa <carlos.feitosa.nt@gmail.com>
2020-08-15 23:49:29 -0300

    initial commit

---
+++ src/main/java/com/skull/bff/MpmBffApplication.java
@@ -0,0 +1,13 @@
+package com.skull.bff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpmBffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MpmBffApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skull/bff/MpmBffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skull.bff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MpmBffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
